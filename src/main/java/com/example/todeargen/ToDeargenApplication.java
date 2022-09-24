dd74fa3fdf8d12178f42f9e82069c748718873ac
Kimjipang <happygimy97@naver.com>
2022-09-24 14:20:48 +0900

    초기 셋팅

---
+++ src/main/java/com/example/todeargen/ToDeargenApplication.java
@@ -0,0 +1,13 @@
+package com.example.todeargen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDeargenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDeargenApplication.class, args);
+    }
+
+}
